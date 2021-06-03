5907a946d0059a00ba19a112e13714d428be5182
rajesh sangareddy <1231rajesh.sangareddy@gmail.com>
2021-06-03 14:12:33 +0530

    first adding

---
+++ src/main/java/com/git/process/rajeshsangareddygit/RajeshSangareddyGitApplication.java
@@ -0,0 +1,13 @@
+package com.git.process.rajeshsangareddygit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajeshSangareddyGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajeshSangareddyGitApplication.class, args);
+	}
+
+}
